8f2f25f8d89eab1dd6dfc4309785c241399c060c
Roman <apanasevich161@me.com>
2022-10-03 16:48:43 +0300

    Commit

---
+++ RouteTask/Alert.swift
@@ -0,0 +1,36 @@
+
+import UIKit
+
+extension UIViewController {
+    
+    func alertAddAdress(title: String, placeholder: String, completionHandler: @escaping (String) -> Void) {
+        
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let alertOk = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+            let tfText = alertController.textFields?.first
+            guard let text = tfText?.text else { return }
+            completionHandler(text)
+        }
+        
+        let alertCancel = UIAlertAction(title: "Отмена", style: .default){ (_) in
+        }
+        
+        alertController.addTextField { (tf) in
+            tf.placeholder = placeholder
+        }
+        
+        alertController.addAction(alertOk)
+        alertController.addAction(alertCancel)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func alertError(title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertOk = UIAlertAction(title: "Ok", style: .default)
+        alertController.addAction(alertOk)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
